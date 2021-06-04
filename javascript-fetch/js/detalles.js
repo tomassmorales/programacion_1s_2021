19073aed3d2570bb61758a4c5e4142d7006d5032
tomassmorales <tomassmorales2707@gmail.com>
2021-06-04 17:29:17 -0300

    primera parte de webstorage

--- javascript-fetch/js/detalles.js
+++ javascript-fetch/js/detalles.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
     let informacionGif = document.querySelector(`#info`)
 
@@ -6,36 +6,72 @@ window.addEventListener("load", function(){
     let queryStringObj = new URLSearchParams(queryString);
     let seleccionado = queryStringObj.get(`id`);
 
+    let favoritos = [];
+    let recuperoStorage = localStorage.getItem(`favoritos`);
+    
+    //Si hay elementos dentro lo pasamos a JSON para 
+    if (recuperoStorage !== null){
+        favoritos = JSON.parse(recuperoStorage);
+    }
+
     console.log(seleccionado);
 
     fetch(`https://api.giphy.com/v1/gifs/${seleccionado}?api_key=wsJgj29OIKytTFMhSBprXKJIpvG9UKeE`)
-    .then(function(response){
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data);
-        let gifSelec = data.data;
-        console.log(gifSelec);
-        let tituloGif = gifSelec.title;
-        let urlGif = gifSelec.images.downsized_large.url;
-        let username = gifSelec.username;
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let gifSelec = data.data;
+            console.log(gifSelec);
+            let id = gifSelec.id;
+            let tituloGif = gifSelec.title;
+            let urlGif = gifSelec.images.downsized_large.url;
+            let username = gifSelec.username;
 
-        if (username == ""){
-            informacionGif.innerHTML += `
+            if (username == "") {
+                informacionGif.innerHTML += `
             <h1>${tituloGif}</h1>
             <h2>Sin nombre de usuario</h2>
             <img src="${urlGif}" alt="${tituloGif}">
             `
-        }else {
-            informacionGif.innerHTML += `
+            } else {
+                informacionGif.innerHTML += `
             <h1>${tituloGif}</h1>
             <h2>${username}</h2>
             <img src="${urlGif}" alt="${tituloGif}">
             `
-        }
-    })
-    .catch(function(error){
-        console.log(error);
-    })
+            }
 
+            if (favoritos.includes(id)){
+                informacionGif.innerHTML += `
+                <a href="" class="sacarFav">Quitar de favoritos</a>
+                `;
+                let botonQuitar = document.querySelector(`.sacarFav`);
+                botonQuitar.addEventListener ("click", function(e){
+                    e.preventDefault();
+                    let gifEliminar = favoritos.indexOf(`${id}`);
+                    favoritos.splice(gifEliminar, 1);
+                })
+            }else {
+                informacionGif.innerHTML += `
+                <a href="" class="fav">Agregar a favoritos</a>
+                `;
+                let botonAgregar = document.querySelector(`.fav`);
+
+                botonAgregar.addEventListener("click", function(e){
+                    e.preventDefault();
+                    favoritos.push(`${id}`);
+                    botonAgregar.classList.remove(`fav`);
+                    botonAgregar.classList.add(`sacarFav`);
+                    botonAgregar.innerText = `Quitar de la lista`;
+                    console.log(favoritos);
+                })
+            }
+
+            let stringFavs = JSON.stringify
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 })
